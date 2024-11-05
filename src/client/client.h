34596149e508700b4e5534cd7a1e53ca5639da2c
SantiNG101 <santinarta101@gmail.com>
2024-11-05 19:11:11 -0300

    Errors fixed for client

--- src/client/client.h
+++ src/client/client.h
@@ -1,7 +1,7 @@
 #ifndef CLIENT_H
 #define CLIENTE_H
 
-#include "parser.h"
+#include "../ignore/parser.h"
 
 typedef enum {
     VALIDATION = 1,
@@ -10,18 +10,16 @@ typedef enum {
 } status;
 
 struct users {
-
     char *user;
     char *pass;
+};
 
-}
-
-struct Client{
+typedef struct Client{
     status state;
     struct users* user;
     int total_mail;
     int not_deleted_mail;
-};
+} Client;
 
 Client* initializing_client();
 void free_client(Client* client);
