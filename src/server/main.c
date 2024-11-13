fd0ac1783fce8d8281af0729f8971f3741801f00
Patricklt2 <108370425+Patricklt2@users.noreply.github.com>
2024-11-13 11:39:14 -0300

    added IPV6 and IPV4 support

--- src/server/main.c
+++ src/server/main.c
@@ -161,17 +161,25 @@ static char* users[] = {
 
 int main() {
     int server_socket;
-    struct sockaddr_in server_addr;
-    
-    server_socket = socket(AF_INET, SOCK_STREAM | SOCK_NONBLOCK, 0);
+    struct sockaddr_in6 server_addr;
+
+    server_socket = socket(AF_INET6, SOCK_STREAM | SOCK_NONBLOCK, 0);
     if (server_socket == -1) {
         perror("Socket creation failed");
         exit(EXIT_FAILURE);
     }
 
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = INADDR_ANY;
-    server_addr.sin_port = htons(PORT);
+    int opt = 0;
+    if (setsockopt(server_socket, IPPROTO_IPV6, IPV6_V6ONLY, &opt, sizeof(opt)) < 0) {
+        perror("Failed to set IPV6_V6ONLY");
+        close(server_socket);
+        exit(EXIT_FAILURE);
+    }
+
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin6_family = AF_INET6;
+    server_addr.sin6_addr = in6addr_any; 
+    server_addr.sin6_port = htons(PORT);
 
     if (bind(server_socket, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
         perror("Binding failed");
@@ -184,7 +192,7 @@ int main() {
         close(server_socket);
         exit(EXIT_FAILURE);
     }
-    printf("Server started on port %d...\n", PORT);
+    printf("Server started on port %d, accepting IPv4 and IPv6 connections...\n", PORT);
 
     init_pollfds();
     init_clientData();
@@ -205,7 +213,9 @@ int main() {
                 pollfds[i].revents = 0;
 
                 if (pollfds[i].fd == server_socket) {
-                    int client_socket = accept(server_socket, NULL, NULL);
+                    struct sockaddr_storage client_addr;
+                    socklen_t addr_len = sizeof(client_addr);
+                    int client_socket = accept(server_socket, (struct sockaddr*)&client_addr, &addr_len);
                     if (client_socket < 0) {
                         perror("Accept failed");
                     } else {
@@ -218,7 +228,7 @@ int main() {
                         }
                     }
                 } else {
-                        handle_client(pollfds[i].fd, NULL, &clients[i]);
+                    handle_client(pollfds[i].fd, NULL, &clients[i]);
                 }
             }
         }
@@ -227,4 +237,3 @@ int main() {
     close(server_socket);
     return 0;
 }
-
